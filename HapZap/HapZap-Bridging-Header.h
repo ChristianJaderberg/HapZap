3e774953f1bfdc7f9b7935a68d9979ef541805bd
Christian Jäderberg <christianjaderberg@gmail.com>
2020-02-28 13:54:47 +0100

    Spotify SDK installed
    The Spotify iOS-framework SDK was installed

---
+++ HapZap/HapZap-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  HapZap-Bridging-Header.h
+//  HapZap
+//
+//  Created by Christian Jäderberg on 2020-02-28.
+//  Copyright © 2020 Christian Jäderberg. All rights reserved.
+//
+
+#ifndef HapZap_Bridging_Header_h
+#define HapZap_Bridging_Header_h
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* HapZap_Bridging_Header_h */
