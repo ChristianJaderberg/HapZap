470d35aaf356abda9b72ab4995f6aa4b7c6db83a
Christian Jäderberg <christianjaderberg@gmail.com>
2020-03-12 11:54:57 +0100

    Updated UI
    Updated UI, album image now shows

--- HapZap/Controllers/HapZapViewController.swift
+++ HapZap/Controllers/HapZapViewController.swift
@@ -13,10 +13,11 @@ class HapZapViewController: UIViewController {
     var randomSongController = RandomSongController()
     var initQuestion = ""
     
-    @IBOutlet weak var questionTextField: UITextField!
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var albumImageView: UIImageView!
     @IBOutlet weak var songNameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var questionTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +39,6 @@ class HapZapViewController: UIViewController {
                     self.updateHapZapUI()
                     print("Question: \"" + self.randomSongController.getQuestion() + "\"")
                     print("HapZapped song: \"\(self.randomSongController.getSongName())\" with \"\(self.randomSongController.getArtistName())\"")
-                    print("AlbumImage url: \(self.randomSongController.getAlbumImage(size: 640))")
                 }
             case .failure(let error): print("Error \(error)")
             }
@@ -46,9 +46,31 @@ class HapZapViewController: UIViewController {
     }
     
     func updateHapZapUI() -> Void {
+        self.setAlbumImage()
         self.questionLabel.text = self.randomSongController.getQuestion()
         self.songNameLabel.text = self.randomSongController.getSongName()
-        self.artistNameLabel.text = self.randomSongController.getArtistName()
+        self.artistNameLabel.text = self.randomSongController.getArtistName().uppercased()
     }
     
+    func setAlbumImage() {
+        let url = self.randomSongController.getAlbumImage(size: 640)
+        guard let imageURL = URL(string: url) else { return }
+        self.albumImageView.downloadImage(from: imageURL)
+    }
+    
+}
+
+extension UIImageView {
+   func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+      URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+   }
+   func downloadImage(from url: URL) {
+      getData(from: url) {
+         data, response, error in
+         guard let data = data, error == nil else { return }
+         DispatchQueue.main.async() {
+            self.image = UIImage(data: data)
+         }
+      }
+   }
 }
